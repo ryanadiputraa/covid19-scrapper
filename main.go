9647c63b9e72f185f11d6617b8eb3bb226adbcc4
Ryan Adi Putra <ryannadiputraa@gmail.com>
2021-06-11 21:08:59 +0800

    serve data with REST API

--- main.go
+++ main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"strconv"
 	"strings"
 
 	"github.com/gocolly/colly"
 )
 
-type WorldData struct {
+type World struct {
 	TotalCoutry		int		`json:"totalCountry"`
 	ConfirmedCase	int		`json:"confirmedCased"`
 	Deaths			int		`json:"deaths"`
+}
+
+type Indonesia struct {
+	Positive	int		`json:"positive"`
+	Recover		int		`json:"recover"`
+	Deaths		int		`json:"deaths"`	
+}
+
+type ResponseData struct {
+	WorldCase 		World		`json:"worldCase"`
+	IndonesiaCase 	Indonesia	`json:"indonesiaCase"`
+}
+
+type Response struct {
+	Code	int				`json:"code"`
+	Data	interface{}		`json:"data"`
 }
 
 func main() {
@@ -44,5 +62,36 @@ func main() {
 		cleanData = append(cleanData, d)
 	}
 
-	fmt.Println(cleanData)
+	worldCase := World {
+		TotalCoutry: cleanData[0],
+		ConfirmedCase: cleanData[1],
+		Deaths: cleanData[2],
+	}
+
+	indonesiaCase := Indonesia {
+		Positive: cleanData[3],
+		Recover: cleanData[4],
+		Deaths: cleanData[5],
+	}
+
+	http.HandleFunc("/api/covidcases", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Accept", "application/json")
+
+		response := Response {
+			Code: http.StatusOK,
+			Data: ResponseData {worldCase, indonesiaCase},
+		}
+
+		enc, err := json.Marshal(response)
+		if err != nil {
+			w.Header().Set("Status", strconv.Itoa(http.StatusInternalServerError))
+			w.Write([]byte("cant get covid cases"))
+		}
+
+		w.Header().Set("Status", strconv.Itoa(http.StatusOK))
+		w.Write(enc)
+	})
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
