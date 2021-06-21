11c037f7ad634c1381b1d963f70ce7e56dc4ded7
Ryan Adi Putra <ryannadiputraa@gmail.com>
2021-06-21 13:23:26 +0800

    refactor rest api and readme

--- README.md
+++ README.md
@@ -1,28 +1,49 @@
 # covid19-scrapper
+
 simple REST API that serve covid case data from covid19.go.id
 
-response example :
-- method: `GET`
-- endpoint: `/api/covidcases`
-- header: 
-  - Content-Type: `application/json`
-  - Accept: `application/json`
-- body:
+---
+
+## API SPEC
+
+### Global Cases
+
+-   method: `GET`
+-   endpoint: `/api/covidcases`
+-   header:
+    -   Content-Type: `application/json`
+    -   Accept: `application/json`
+-   body:
+
 ```json
 {
-  code: 200,
-  data: {
-    worldCase: {
-      totalCountry: 162,
-      confirmedCased: 119611553,
-      deaths: 2605995
-    },
-    indonesiaCase: {
-      positive: 1894025,
-      recover: 1735144,
-      deaths: 52566
-      }
+    "code": 200,
+    "data": {
+        "totalCountry": 162,
+        "confirmedCased": 119611553,
+        "deaths": 2605995
     }
-  }
+}
+```
+
+---
+
+### Indonesia Cases
+
+-   method: `GET`
+-   endpoint: `/api/covidcases`
+-   header:
+    -   Content-Type: `application/json`
+    -   Accept: `application/json`
+-   body:
+
+```json
+{
+    "code": 200,
+    "data": {
+        "positive": 1894025,
+        "recover": 1735144,
+        "deaths": 52566
+    }
 }
 ```
--- main.go
+++ main.go
@@ -23,11 +23,6 @@ type Indonesia struct {
 	Deaths		int		`json:"deaths"`	
 }
 
-type ResponseData struct {
-	WorldCase 		World		`json:"worldCase"`
-	IndonesiaCase 	Indonesia	`json:"indonesiaCase"`
-}
-
 type Response struct {
 	Code	int				`json:"code"`
 	Data	interface{}		`json:"data"`
@@ -62,25 +57,49 @@ func main() {
 		cleanData = append(cleanData, d)
 	}
 
-	worldCase := World {
+	globalCases := World {
 		TotalCoutry: cleanData[0],
 		ConfirmedCase: cleanData[1],
 		Deaths: cleanData[2],
 	}
 
-	indonesiaCase := Indonesia {
+	indonesiaCases := Indonesia {
 		Positive: cleanData[3],
 		Recover: cleanData[4],
 		Deaths: cleanData[5],
 	}
 
-	http.HandleFunc("/api/covidcases", func(w http.ResponseWriter, r *http.Request) {
+
+	// REST API
+
+	// global cases
+	http.HandleFunc("/api/global", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Accept", "application/json")
 
 		response := Response {
 			Code: http.StatusOK,
-			Data: ResponseData {worldCase, indonesiaCase},
+			Data: globalCases,
+		}
+
+		enc, err := json.Marshal(response)
+		if err != nil {
+			w.Header().Set("Status", strconv.Itoa(http.StatusInternalServerError))
+			w.Write([]byte("cant get covid cases"))
+		}
+
+		w.Header().Set("Status", strconv.Itoa(http.StatusOK))
+		w.Write(enc)
+	})
+
+	// indonesia cases
+	http.HandleFunc("/api/indonesia", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Accept", "application/json")
+
+		response := Response {
+			Code: http.StatusOK,
+			Data: indonesiaCases,
 		}
 
 		enc, err := json.Marshal(response)
